a566e1d0fbee8f127e84b2da1cadae69057be493
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-02-21 17:36:34 +0530

    Implemented Customer class, initialized Reg Queue

---
+++ Project2/Customer.hpp
@@ -0,0 +1,11 @@
+class Customer{
+  public:
+    int time;    //Time of entering to Lane
+    int items;   //Number of items
+    //Parameterized constructor
+    Customer(int t, int i){
+      time = t;
+      items = i;
+    }
+
+}
